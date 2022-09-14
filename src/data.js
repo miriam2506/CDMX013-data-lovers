3b02e9e63fa410ce44381bc3596570cabba4ed7c
Diazdias <109100785+Diazdias@users.noreply.github.com>
2022-09-14 08:14:52 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,59 @@
-// estas funciones son de ejemplo
+export const filterByDirector = (films, directorName) => {
+  const Directors = films.filter(film => {
+    return film.director === directorName
+  })
+  return Directors
+}
 
-export const example = () => {
-  return 'example';
-};
+export const filterByProducer = (films, producerName) => {
+  const Producers = films.filter(film => {
+    return film.producer === producerName
+  })
+  return Producers
+}
 
-export const anotherExample = () => {
-  return 'OMG';
+export const filterByYear = (films, releaseDate) => {
+  const Years = films.filter(film => {
+    return film.release_date === releaseDate
+  })
+  return Years
+}
+
+export function sortFilms(films, orderOption) {
+  if (orderOption === "A to Z") {
+    return [...films].sort((a, b) => {
+      if (a.title < b.title) {
+        return -1
+      }
+      else if (a.title > b.title) {
+        return 1
+      }
+      return 0
+    });
+  }
+  else if (orderOption === "Z to A") {
+    return [...films].sort((b, a) => {
+      if (a.title < b.title) {
+        return -1
+      }
+      if (a.title > b.title) {
+        return 1
+      }
+      return 0
+    });
+  }
+}
+
+export const getAverageScore = (films) => {
+  let acumulator = 0
+  films.forEach(film => {
+    acumulator+=Number(film.rt_score)
+  
+  })
+  
+  let division= acumulator / films.length
+let result=Math.round(division)
+let total =result + "%"
+return (total)
+
 };
