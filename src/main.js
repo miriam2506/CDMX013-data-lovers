2b36cfe882e6eebd3d30578360522edb09ba33dd
Diazdias <109100785+Diazdias@users.noreply.github.com>
2022-09-14 08:13:07 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,78 @@
-import allData from'./data/ghibli/ghibli.js'
-const root=document.getElementById('root')
+import allData from './data/ghibli/ghibli.js'
+import { filterByDirector, filterByProducer, filterByYear, sortFilms, getAverageScore } from './data.js'
 
-const films=allData.films
+const root = document.getElementById('root')
 
-const generadorHTML=(films)=>{
-    
-    const div=document.createElement('div')
-    div.classList='filmStyle'
+const films = allData.films
 
-    const title=document.createElement('h2')
-    title.textContent=films.title
+const generatorHTML = (films) => {
 
-    const year=document.createElement('h5')
-    year.textContent=films.release_date
+    const div = document.createElement('div')
+    div.classList = 'filmStyle'
 
-    const poster= document.createElement('img')
-    poster.setAttribute ("src",films.poster)
+    const title = document.createElement('h2')
+    title.textContent = films.title
 
-    div.append(poster,title,year)
+    const year = document.createElement('h5')
+    year.textContent = films.release_date
+
+    const poster = document.createElement('img')
+    poster.setAttribute("src", films.poster)
+
+    div.append(poster, title, year)
     return div
 }
-films.forEach(oneFilm=>root.appendChild(generadorHTML(oneFilm))) 
+films.forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+
+document.getElementById('filterDirector').addEventListener('change', function (e) {
+
+    root.innerHTML = ""
+    if (e.target.value == "All Directors") {
+        films.forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+    else {
+        filterByDirector(films, e.target.value).forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+})
+
+
+document.getElementById('filterProducer').addEventListener('change', function (e) {
+    root.innerHTML = ""
+    if (e.target.value == "All Producers") {
+        films.forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+    else {
+        filterByProducer(films, e.target.value).forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+})
+
+document.getElementById('filterReleaseyear').addEventListener('change', function (e) {
+
+    root.innerHTML = ""
+    if (e.target.value == "Year") {
+        films.forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+    else {
+        filterByYear(films, e.target.value).forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+})
+
+document.getElementById('selectOrder').addEventListener('change', function (e) {
+    root.innerHTML = ""
+
+    if (e.target.value === "Order") {
+        films.forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+    else {
+        sortFilms(films, e.target.value).forEach(oneFilm => root.appendChild(generatorHTML(oneFilm)))
+    }
+});
+
+const stadistic = document.querySelector('#random')
+stadistic.addEventListener('click', function () {
+    const screen = getAverageScore(films)
+    root.innerHTML = `<h6>Did you know that?
+<p>The average of Studio Ghibli films </p>
+<p>acceptances is of ${screen} </p>
+</h6> `;
+});
